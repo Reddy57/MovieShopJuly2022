09b2a5025f2eb2bc407e898275ae083ebbee5d0a
Reddy57 <abhilash.gaddam@antra.com>
2022-08-23 14:45:41 -0400

    Add project files.

---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    // Controller
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Action methods
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       // http://movieshop.com/home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
