2e788ccb148b06cb5e285c69e54eeea4e2cd5dbc
Reddy57 <abhilash.gaddam@antra.com>
2022-08-24 16:08:00 -0400

    Created first table and migration

--- Infrastructure/Data/MovieShopDbContext.cs
+++ Infrastructure/Data/MovieShopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,10 @@ namespace Infrastructure.Data
         {
 
         }
+
+        // Dbsets are properties of DbContext class
+
+        // Movies Table access
+        public DbSet<Movie> Movies { get; set; } 
     }
 }
