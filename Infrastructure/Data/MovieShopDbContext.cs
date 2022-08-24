eb4750df590ac486de8dde0b589918d60be409c7
Reddy57 <abhilash.gaddam@antra.com>
2022-08-24 16:46:40 -0400

    Used Fluent API for Movies Table

--- Infrastructure/Data/MovieShopDbContext.cs
+++ Infrastructure/Data/MovieShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,45 @@ namespace Infrastructure.Data
         // Dbsets are properties of DbContext class
 
         // Movies Table access
-        public DbSet<Movie> Movies { get; set; } 
+        public DbSet<Movie> Movies { get; set; }
+
+        // override the method called OnModelCreating for Fluent API
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(ConfigureMovie);
+        }
+
+        private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
+        {
+            // specify all the Fluent API rules
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Title).HasMaxLength(512);
+            builder.Property(m => m.Overview).HasMaxLength(4096);
+            builder.Property(m => m.Tagline).HasMaxLength(512);
+            builder.Property(m => m.ImdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.TmdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.PosterUrl).HasMaxLength(2084);
+            builder.Property(m => m.BackdropUrl).HasMaxLength(2084);
+            builder.Property(m => m.OriginalLanguage).HasMaxLength(64);
+            builder.Property(m => m.UpdatedBy).HasMaxLength(512);
+            builder.Property(m => m.CreatedBy).HasMaxLength(512);
+
+            builder.Property(m => m.Price).HasColumnType("decimal(5, 2)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Budget).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Revenue).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
+
+            // you want the property in your C# for your business logic not as column in database
+            builder.Ignore(m => m.Rating);
+
+            builder.HasIndex(m => m.Title);
+            builder.HasIndex(m => m.Price);
+            builder.HasIndex(m => m.Revenue);
+            builder.HasIndex(m => m.Budget);
+
+
+        }
+
     }
 }
